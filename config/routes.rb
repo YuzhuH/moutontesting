bfdc24d30456e1cf8a8faddbc90950f51236257d
Yuzhu Hou <yuzhu.hou@gmail.com>
2018-01-11 16:52:30 +0100

    connected navbar link to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'projects', to: 'pages#projects'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
